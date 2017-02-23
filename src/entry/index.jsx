84f2c2f49b56868db312410e2fb5c710d8803993
carlos121493 <guowei@shnow.cn>
2017-02-23 17:35:06 +0800

    添加ready

--- src/entry/index.jsx
+++ src/entry/index.jsx
@@ -16,9 +16,20 @@ const RouteCollection = RouteArray.map((props, index) =>
   <Route {...props} key={index} />
 );
 
-ReactDOM.render(
+function ready(callback) {
+  if (window.AlipayJSBridge && callback) {
+    callback();
+  } else {
+    document.addEventListener('AlipayJSBridgeReady', callback);
+  }
+}
+
+ready(() => {
+  ReactDOM.render(
   <Router history={hashHistory}>
     {RouteCollection}
   </Router>,
-  document.getElementById('react-content')
-);
+    document.getElementById('react-content')
+  );
+});
+
